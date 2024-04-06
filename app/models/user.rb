9229ff95e96771f79196e42264096824024ded50
Jyoumama <juri19771211@gmail.com>
2024-04-07 01:45:42 +0900

    user_spec.rb単体テストコードの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, presence: true
-  validates :password, length: { minimum: 6 }
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
+  with_options presence: true do
+    validates :nickname 
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+    validates :birth_date
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.first_name.slice(0, 40) }
+    email {Faker::Internet.email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name { Faker::Name.first_name}
+    last_name { Faker::Name.last_name }
+    last_name_kana { "カタカナ" }
+    first_name_kana { "カタカナ" }
+    birth_date { Faker::Date.birthday(min_age: 5, max_age: 94) }
+  end
+end
